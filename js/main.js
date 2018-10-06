d20402f62b857f144f05c626ca15ffa8a1fbf9af
yuqi chen <chenyuqide@outlook.com>
2018-10-06 20:05:30 +0800

    Add cursor and a info bar to show position.

--- js/main.js
+++ js/main.js
@@ -1,97 +1,27 @@
-function CurrentStatus() {
-    this.mouse = {
-        X: 0,
-        Y: 0,
-        moved: true,
-        update: function (pageX, pageY) {
-            this.X = pageX - NE.pad.offset().left;
-            this.Y = pageY - NE.pad.offset().top;
-        }
-    };
-
-    this.scaleindex = 20;
-    this.scale = 1;
-
-    this.area = "normal";
-    this.minimapRate = 0.1;
-
-}
-
 $(document).ready(function () {
     NE = new NodeEditor();
-    CS = new CurrentStatus();
+    CS = NE.cs;
 
     $(document).mousemove(function (e) {
         // updata mouse position.
         CS.mouse.update(e.pageX, e.pageY);
 
         // right button to move pad.
-        if (e.which == 3) {
-            var dx = e.originalEvent.movementX;
-            var dy = e.originalEvent.movementY;
+        if (e.which == 3)
+            NE.view.moveE(e);
+    });
 
-            if (dx == 0 && dy == 0)
-                CS.mouse.move = false;
-            else
-                CS.mouse.move = true;
-
-            // move background and pad.
-            NE.bg.move(dx, dy);
-            NE.pad.move(dx, dy);
-        }
+    $(document).mousedown(function (e) {
+        if (e.which == 1)
+            NE.cursor.move(CS.mouse.X, CS.mouse.Y)
     });
 
     $(window).resize(function () {
-        editorViewReset();
+        NE.view.reset();
     });
 
     $(document).contextmenu(function (e) {
         if (CS.mouse.moved)
             e.preventDefault();
     });
-
 })
-
-
-// // Reset minimap view scale and position.
-// function minimapViewReset() {
-//     NE.minimapView.style.width = CS.miniW * CS.areaW / CS.windowW / CS.scale + "px";
-//     NE.minimapView.style.height = CS.miniH * CS.areaH / CS.windowH / CS.scale + "px";
-
-//     NE.minimapView.style.left = NE.editorView.scrollLeft * CS.minimapRate + "px";
-//     NE.minimapView.style.top = NE.editorView.scrollTop * CS.minimapRate + "px";
-// }
-
-// // Scale editor area.
-// function scale(times) {
-//     var di = times > 0 ? 1 : -1;
-//     var newIndex = CS.scaleindex + di;
-//     newIndex = newIndex < 0 ? 0 : newIndex;
-//     newIndex = newIndex >= NE.scaleLevel.length ? NE.scaleLevel.length - 1 : newIndex;
-
-//     var newScale = NE.scaleLevel[newIndex];
-//     var oldScale = NE.scaleLevel[CS.scaleindex]
-
-//     CS.scaleindex = newIndex
-//     CS.scale = newScale;
-//     NE.editorArea.style.transform = "scale(" + CS.scale + "," + CS.scale + ")";
-
-//     NE.editorView.scrollLeft += (CS.mouseX - CS.areaW / 2) * (newScale - oldScale);
-//     NE.editorView.scrollTop += (CS.mouseY - CS.areaH / 2) * (newScale - oldScale);
-
-//     minimapViewReset();
-// }
-
-
-// document.addEventListener('wheel', function (e) {
-//     if (CS.area != "normal")
-//         e.preventDefault();
-//     if (e.shiftKey) {
-//         scale(-e.deltaY)
-//     }
-//     e.preventDefault();
-// })
-
-// document.addEventListener("show", function (e) {
-//     e.preventDefault();
-// })
--- js/nodeeditor.js
+++ js/nodeeditor.js
@@ -1,15 +1,30 @@
 function NodeEditor() {
     this.view = $("#editor_view");
+    this.view.p = this;
     // Reset editor View.
     this.view.reset = function () {
         this.height(window.innerHeight)
         this.width(window.innerWidth)
-    }
+    };
     this.view.back = function () {
-        NE.pad.moveTo(NE.view.width() / 2, NE.view.height() / 2)
+        this.p.pad.moveTo(this.width() / 2, this.height() / 2)
+        this.p.bg.moveTo(this.width() / 2, this.height() / 2)
+    };
+    this.view.moveE = function (e) {
+        this.move(e.originalEvent.movementX, e.originalEvent.movementY)
     }
+    this.view.move = function (dx, dy) {
+        if (dx == 0 && dy == 0)
+            this.p.cs.mouse.move = false;
+        else
+            this.p.cs.mouse.move = true;
+        // move background and pad.
+        this.p.bg.move(dx, dy);
+        this.p.pad.move(dx, dy);
+    };
 
     this.pad = $("#pad");
+    this.pad.p = this;
     this.pad.move = function (dx, dy) {
         this.css({
             "left": function (i, v) { return parseInt(v, 10) + dx + "px"; },
@@ -36,8 +51,37 @@ function NodeEditor() {
             return x + "px " + y + "px";
         });
     };
+    this.cursor = $("#cursor");
+    this.cursor.p = this;
+    this.cursor.move = function (x, y) {
+        this.css("left", x - this.width() / 2 + "px");
+        this.css("top", y - this.height() / 2 + "px");
+    };
+    this.cs = new CurrentStatus();
 
     this.map = { view: $("#map"), pad: $("#map_pad"), bg: $("#map_background") };
     this.scaleLevel = [0.25, 0.267943366, 0.287174589, 0.307786103, 0.329876978, 0.353553391, 0.378929142, 0.406126198, 0.435275282, 0.466516496, 0.5, 0.535886731, 0.574349177, 0.615572207, 0.659753955, 0.707106781, 0.757858283, 0.812252396, 0.870550563, 0.933032992, 1, 1.071773463, 1.148698355, 1.231144413, 1.319507911, 1.414213562, 1.515716567, 1.624504793, 1.741101127, 1.866065983, 2, 2.143546925, 2.29739671, 2.462288827, 2.639015822, 2.828427125, 3.031433133, 3.249009585, 3.482202253, 3.732131966, 4, 4.28709385, 4.59479342, 4.924577653, 5.278031643, 5.656854249, 6.062866266, 6.498019171, 6.964404506, 7.464263932, 8, 8.5741877, 9.18958684, 9.849155307, 10.55606329, 11.3137085, 12.12573253, 12.99603834, 13.92880901, 14.92852786, 16, 17.1483754, 18.37917368, 19.69831061, 21.11212657, 22.627417, 24.25146506, 25.99207668, 27.85761803, 29.85705573, 32];
+
     this.view.reset();
+    this.view.back();
+    this.cursor.move(0, 0);
+}
+
+function CurrentStatus() {
+    this.mouse = {
+        X: 0,
+        Y: 0,
+        moved: true,
+        update: function (pageX, pageY) {
+            this.X = pageX - NE.pad.offset().left;
+            this.Y = pageY - NE.pad.offset().top;
+            $("#pos").html("X:" + this.X + "Y:" + this.Y);
+        }
+    };
+
+    this.scaleindex = 20;
+    this.scale = 1;
+
+    this.area = "normal";
+    this.minimapRate = 0.1;
 }
